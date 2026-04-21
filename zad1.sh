2546609138011fb955b8ff779672849c1d1a722e
panasinskipiotr <piotrp1003@gmail.com>
2026-04-21 09:53:05 +0200

    Laboratorium 2

--- zad1.sh
+++ zad1.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-IMIE="Piotr"
-echo $IMIE
-export IMIE
+read -p "Podaj nazwe: " nazwa_pliku
+
+if [ ! -e "$nazwa_pliku" ]; then
+	echo "Plik nie istnieje!"
+elif [ -d "$nazwa_pliku" ]; then
+	echo "Plik istnieje i jest katalogiem!"
+elif [ -f "$nazwa_pliku" ]; then
+	echo "Plik istnieje i jest zwyklym plikem!"
+else
+	echo "Nieznany typ!"
+fi
--- zad3.sh
+++ zad3.sh
@@ -1,2 +1,10 @@
-#Zmiana prompta
-PS1="TERMINAL> "
+!/bin/bash
+read -p "Podaj nazwę pliku: " nazwa_pliku
+
+if [ -f "$nazwa_pliku" ] && [ -x "$nazwa_pliku" ]; then
+    ./"$nazwa_pliku"
+    wynik=$?
+    echo "Program zakończony kodem wyjścia: $wynik"
+else
+    echo "Plik nie jest plikiem zwykłym lub nie ma praw do wykonywania!"
+fi
